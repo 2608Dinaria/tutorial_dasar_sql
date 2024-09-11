b1bd6e56f346edd5d08ba99819e976947be25cdc
Dinaria Simatupang <dinariasimatupang@students.polmed.ac.id>
2024-09-10 13:30:59 -1200

    Add files via upload

---
+++ tutorial_dasar_sql/select_statement.sql
@@ -0,0 +1,17 @@
+**/ mencari jenis kamar dan harga per malam yang memiliki kapasitas 4 /
+SELECT 
+      jenis_kamar,
+      harga_per_malam
+FROM 
+   tb_kamar 
+WHERE 
+   kapasitas = 4;
+
+**// mencari alamat dan nama pelanggan yang bernama Kevin Sanjaya /
+SELECT 
+     nama,
+     alamat
+FROM 
+   tb_pelanggan
+WHERE 
+   nama = "Kevin Sanjaya";
